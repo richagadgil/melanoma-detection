0dad9816b92056729f50d7e94471eef94a2124d4
Waidhoferj <jwaidhof@calpoly.edu>
2020-02-29 22:14:16 -0800

    post hits gcloud

--- gcp_automl.py
+++ gcp_automl.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import base64
 
 from google.cloud import automl_v1beta1
 from google.cloud.automl_v1beta1.proto import service_pb2
@@ -13,27 +14,25 @@ CORS(app)
 BAD_REQUEST = 400
 SUCCESS = 200
 
+project_id = 414357853263
+model_id = "ICN5748246790012928"
+
 # 'content' is base-64-encoded image data.
 @app.route("/submit-image", methods=["POST"])
-def get_prediction(content, project_id, model_id):
+def get_prediction():
+    image = request.files["image"]
+    image_b64 = base64.b64encode(image.read())
     prediction_client = automl_v1beta1.PredictionServiceClient()
 
     name = "projects/{}/locations/us-central1/models/{}".format(project_id, model_id)
-    payload = {"image": {"image_bytes": content}}
+    payload = {"image": {"image_bytes": image_b64}}
     params = {}
-    request = prediction_client.predict(name, payload, params)
-    return request  # waits till request is returned
+    response = prediction_client.predict(name, payload, params)
+    return response  # waits till request is returned
 
 
 if __name__ == "__main__":
-    file_path = sys.argv[1]
-    project_id = sys.argv[2]
-    model_id = sys.argv[3]
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(
         os.getcwd(), "slohacks.json"
     )
-
-    with open(file_path, "rb") as ff:
-        content = ff.read()
-    print(type(content))
-    print(get_prediction(content, project_id, model_id))
+    app.run(debug=True)
